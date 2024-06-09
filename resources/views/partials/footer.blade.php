994f86800882d46097cc9aa6bb7887b22d87f9c0
HATIM ILYAS <125820069+HatimIlyas04@users.noreply.github.com>
2024-06-09 16:11:34 +0200

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
-<link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+<link rel="stylesheet" href="{{ asset('css/footer.css') }}">
 
-<footer class="footer">
+<footer class="footer mt-auto py-3 bg-light">
     <div class="container">
-        <p>&copy; 2024 7ARFTY. Tous droits réservés HATIM ILYAS / TIGUINI AHMED.</p>
+        <p class="text-center mb-0">&copy; 2024 7ARFTY. Tous droits réservés HATIM ILYAS / TIGUINI AHMED.</p>
     </div>
 </footer>
