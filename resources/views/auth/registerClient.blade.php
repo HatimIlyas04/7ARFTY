8c8c839220eecdf31b85c12851dbc1eb04ff97e7
HATIM ILYAS <125820069+HatimIlyas04@users.noreply.github.com>
2024-06-09 16:06:25 +0200

    Update registerClient.blade.php

--- resources/views/auth/registerClient.blade.php
+++ resources/views/auth/registerClient.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
-<link rel="stylesheet" href="{{ asset('css/inscription-client.css') }}">
 
 @section('content')
 <div class="container">
@@ -12,26 +11,26 @@
                 @csrf
                 <div class="form-group">
                     <label for="prenom">Prénom</label>
-                    <input type="text" name="prenom" id="prenom" placeholder="Prénom" required>
+                    <input type="text" class="form-control" name="prenom" id="prenom" placeholder="Prénom" required>
                 </div>
                 <div class="form-group">
                     <label for="nom">Nom</label>
-                    <input type="text" name="nom" id="nom" placeholder="Nom" required>
+                    <input type="text" class="form-control" name="nom" id="nom" placeholder="Nom" required>
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Email" required>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
                 </div>
                 <div class="form-group">
                     <label for="password">Mot de passe</label>
-                    <input type="password" name="password" id="password" placeholder="Mot de passe" required>
+                    <input type="password" class="form-control" name="password" id="password" placeholder="Mot de passe" required>
                 </div>
                 <div class="form-group">
                     <label for="password_confirmation">Confirmation du mot de passe</label>
-                    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirmation du mot de passe" required>
+                    <input type="password" class="form-control" name="password_confirmation" id="password_confirmation" placeholder="Confirmation du mot de passe" required>
                 </div>
                 <div class="form-group">
-                    <button type="submit">S'inscrire</button>
+                    <button type="submit" class="btn btn-primary">S'inscrire</button>
                 </div>
             </form>
         </div>
